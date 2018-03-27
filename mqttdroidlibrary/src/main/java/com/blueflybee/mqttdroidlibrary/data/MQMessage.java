e054abbfce6f1a03cba437ff7de51c0f54156d78
shaojun <wusj2017@gmail.com>
2018-03-27 16:16:25 +0800

    MQMessage实现Parcelable接口

--- mqttdroidlibrary/src/main/java/com/blueflybee/mqttdroidlibrary/data/MQMessage.java
+++ mqttdroidlibrary/src/main/java/com/blueflybee/mqttdroidlibrary/data/MQMessage.java
@@ -1,5 +1,8 @@
 package com.blueflybee.mqttdroidlibrary.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * <pre>
  *     author : shaojun
@@ -9,7 +12,7 @@ package com.blueflybee.mqttdroidlibrary.data;
  *     version: 1.0
  * </pre>
  */
-public class MQMessage {
+public class MQMessage implements Parcelable{
 
   private String topic;
   private String message;
@@ -42,4 +45,31 @@ public class MQMessage {
         ", message=" + message +
         '}';
   }
+
+  @Override
+  public int describeContents() {
+    return 0;
+  }
+
+  @Override
+  public void writeToParcel(Parcel dest, int flags) {
+    // 序列化过程：必须按成员变量声明的顺序进行封装
+    dest.writeString(topic);
+    dest.writeString(message);
+  }
+
+  // 反序列过程：必须实现Parcelable.Creator接口，并且对象名必须为CREATOR
+  // 读取Parcel里面数据时必须按照成员变量声明的顺序，Parcel数据来源上面writeToParcel方法，读出来的数据供逻辑层使用
+  public static final Parcelable.Creator<MQMessage> CREATOR = new Creator<MQMessage>() {
+
+    @Override
+    public MQMessage createFromParcel(Parcel source) {
+      return new MQMessage(source.readString(), source.readString());
+    }
+
+    @Override
+    public MQMessage[] newArray(int size) {
+      return new MQMessage[size];
+    }
+  };
 }
